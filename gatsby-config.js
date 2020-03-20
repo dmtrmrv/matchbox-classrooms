5b11049fe210f385389349714326288edf1be7ee
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2020-03-20 00:11:30 -0400

    Add initial markdown setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `classrooms`,
+        path: `${__dirname}/src/classrooms`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
